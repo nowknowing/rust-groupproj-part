994cf0bd020acddecb16721469eb858a0943b3d8
sshion <s.sunaga00@gmail.com>
2022-03-05 02:52:02 +0800

    left with control flow

---
+++ examples/function/function_return.rs
@@ -0,0 +1,15 @@
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+    println!("The value of x is 5: {}", x);
+    
+    let x = plus_one(x);
+    println!("The value of x is 6: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+x + 1
+}
