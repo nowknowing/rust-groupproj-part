50abcf706b5cd726363f3a18134550fb3f1c91db
sshion <s.sunaga00@gmail.com>
2022-03-05 17:34:24 +0800

    function_ref update

--- examples/function/function_ref.rs
+++ examples/function/function_ref.rs
@@ -10,20 +10,18 @@ fn example2 (x: & i32, y: & i32) -> i32 {
 }
 
 fn example3 (x: & i32, y : & i32) -> i32 {
-    if *x > *y {
-        *x       // dereferencing matters
-    } else {
-        *y      // dereferincing matters
-    }
+    println!("{}, {}", x ,y);
+    *x
+ // dereferencing matters
 }
 
 fn main() {
     let mut x = 1;
     let mut y = 2;
     x = example1(&mut x, &mut y);
+    println!("x is changed to 42: {}", x);
     let sum = example2(&x, &y);
     let larger = example3(&x, &y);
-    println!("x is changed to 42: {}", x);
     println!("sum is now 55: {}", sum);
     println!("larger is : {} than {}", larger, y);
 
