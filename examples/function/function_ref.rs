48026e230cca325b3a4896362cac90f866a0b6d0
sshion <s.sunaga00@gmail.com>
2022-03-05 17:23:21 +0800

    weird dereferencing behavior

--- examples/function/function_ref.rs
+++ examples/function/function_ref.rs
@@ -5,13 +5,26 @@ fn example1 (x: & mut i32 , y: & mut i32 ) -> i32 {
 }
 
 fn example2 (x: & i32, y: & i32) -> i32 {
-    *x + *y
+    x + y
+    // *x + *y   no diff
 }
+
+fn example3 (x: &i32, y : & i32) -> i32 {
+    if *x > *y {
+        *x       // dereferencing matters
+    } else {
+        *y      // dereferincing matters
+    }
+}
+
 fn main() {
     let mut x = 1;
     let mut y = 2;
     x = example1(&mut x, &mut y);
     let sum = example2(&x, &y);
+    let larger = example3(&x, &y);
     println!("x is changed to 42: {}", x);
     println!("sum is now 55: {}", sum);
+    println!("larger is : {} than {}", larger, y);
+    
 }
