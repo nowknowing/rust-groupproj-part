860a5bbba1089a1ffe869cdb775406821929f137
Chua Bing Quan <chipneverdies@gmail.com>
2022-03-30 23:53:40 +0800

    Add parser generator dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct OxidoParser;
+
 fn main() {
-    println!("Hello, world!");
+    let successful_parse = OxidoParser::parse(Rule::function_declaration, "fn main () {}");
+    println!("{:?}", successful_parse);
 }
